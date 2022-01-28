e4d4f0898d8b99320dfabaa56f545593b33927e7
Shashank Sinha <sinhashashank.98@gmail.com>
2022-01-28 13:27:59 +0530

    Referencing Multiple Dagger Dependencies within a Single Module

--- app/src/main/java/com/shashank/dagger2cwm/AuthActivity.kt
+++ app/src/main/java/com/shashank/dagger2cwm/AuthActivity.kt
@@ -1,8 +1,11 @@
 package com.shashank.dagger2cwm
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import com.bumptech.glide.RequestManager
 import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
 import javax.inject.Named
@@ -11,17 +14,22 @@ class AuthActivity : DaggerAppCompatActivity() {
 
     private val TAG = "AuthActivity"
 
-    @Inject
-    lateinit var myString: String
+    @Inject @JvmField
+    var logo: Drawable?=null
 
-    @set:[Inject Named("myBool")]
-    var myBool: Boolean = true
+    @Inject
+    lateinit var requestManager: RequestManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
 
-        Log.d(TAG,"xlr8: $myString")
-        Log.d(TAG,"xlr8: $myBool")
+        setLogo()
+
+    }
+
+    private fun setLogo() {
+        requestManager.load(logo)
+            .into(findViewById<ImageView>(R.id.login_logo))
     }
 }
--- app/src/main/java/com/shashank/dagger2cwm/di/AppModule.kt
+++ app/src/main/java/com/shashank/dagger2cwm/di/AppModule.kt
@@ -1,20 +1,32 @@
 package com.shashank.dagger2cwm.di
 
 import android.app.Application
+import android.graphics.drawable.Drawable
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
+import com.bumptech.glide.request.RequestOptions
+import com.shashank.dagger2cwm.R
 import dagger.Module
 import dagger.Provides
 
 @Module
 class AppModule {
 
-    @Module
-    companion object {
-        @Provides
-        @JvmStatic
-        fun someString(): String = "Built by shashank sinha"
+    @Provides
+    fun provideRequestOptions(): RequestOptions {
+        return RequestOptions.placeholderOf(R.drawable.white_background)
+            .error(R.drawable.white_background)
+    }
 
-        @Provides
-        @JvmStatic
-        fun someBool(application: Application): Boolean = application==null
+    @Provides
+    fun provideGlideInstance(application: Application, requestOptions: RequestOptions): RequestManager {
+        return Glide.with(application)
+            .setDefaultRequestOptions(requestOptions)
+    }
+
+    @Provides
+    fun provideAppDrawable(application: Application): Drawable? {
+        return ContextCompat.getDrawable(application, R.drawable.logo)
     }
 }
