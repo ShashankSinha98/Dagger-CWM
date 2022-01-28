f2758467068f6f06060a0bb47b063a45c0849171
Shashank Sinha <sinhashashank.98@gmail.com>
2022-01-28 13:56:05 +0530

    Singleton Scope with Dagger2

--- app/src/main/java/com/shashank/dagger2cwm/di/AppComponent.kt
+++ app/src/main/java/com/shashank/dagger2cwm/di/AppComponent.kt
@@ -6,12 +6,13 @@ import dagger.BindsInstance
 import dagger.Component
 import dagger.android.AndroidInjector
 import dagger.android.support.AndroidSupportInjectionModule
+import javax.inject.Singleton
 
+@Singleton
 @Component(
     modules = [AndroidSupportInjectionModule::class,
                 ActivityBuildersModule::class,
-                AppModule::class
-    ]
+                AppModule::class]
 )
 interface AppComponent: AndroidInjector<BaseApplication> {
 
--- app/src/main/java/com/shashank/dagger2cwm/di/AppModule.kt
+++ app/src/main/java/com/shashank/dagger2cwm/di/AppModule.kt
@@ -9,22 +9,26 @@ import com.bumptech.glide.request.RequestOptions
 import com.shashank.dagger2cwm.R
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class AppModule {
 
+    @Singleton
     @Provides
     fun provideRequestOptions(): RequestOptions {
         return RequestOptions.placeholderOf(R.drawable.white_background)
             .error(R.drawable.white_background)
     }
 
+    @Singleton
     @Provides
     fun provideGlideInstance(application: Application, requestOptions: RequestOptions): RequestManager {
         return Glide.with(application)
             .setDefaultRequestOptions(requestOptions)
     }
 
+    @Singleton
     @Provides
     fun provideAppDrawable(application: Application): Drawable? {
         return ContextCompat.getDrawable(application, R.drawable.logo)
