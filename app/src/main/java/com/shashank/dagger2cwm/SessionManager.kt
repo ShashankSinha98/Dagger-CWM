74c54885be76f5309fa26aad4739e6437654b8a6
Shashank Sinha <sinhashashank.98@gmail.com>
2022-01-29 21:48:12 +0530

    Managing Authentication with Dagger (part 1/3)

---
+++ app/src/main/java/com/shashank/dagger2cwm/SessionManager.kt
@@ -0,0 +1,37 @@
+package com.shashank.dagger2cwm
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import com.shashank.dagger2cwm.models.User
+import com.shashank.dagger2cwm.ui.auth.AuthResource
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class SessionManager @Inject constructor() {
+    private val TAG = "SessionManager"
+
+    private val cachedUser: MediatorLiveData<AuthResource<User>> = MediatorLiveData()
+
+    fun authenticateWithId(source: LiveData<AuthResource<User>>) {
+        cachedUser.value = AuthResource.loading(null)
+        cachedUser.addSource(source) { userAuthResource: AuthResource<User> ->
+            cachedUser.value = userAuthResource
+            cachedUser.removeSource(source)
+        }
+
+        Log.d(TAG, "authenticateWithId: previous session detected. Retrieving user from cache")
+    }
+
+    fun logOut() {
+        Log.d(TAG,"logOut: logging out...")
+        cachedUser.value = AuthResource.logout()
+    }
+
+    fun getAuthUser(): LiveData<AuthResource<User>> = cachedUser
+
+
+
+
+}
--- app/src/main/java/com/shashank/dagger2cwm/di/AppComponent.kt
+++ app/src/main/java/com/shashank/dagger2cwm/di/AppComponent.kt
@@ -2,6 +2,7 @@ package com.shashank.dagger2cwm.di
 
 import android.app.Application
 import com.shashank.dagger2cwm.BaseApplication
+import com.shashank.dagger2cwm.SessionManager
 import dagger.BindsInstance
 import dagger.Component
 import dagger.android.AndroidInjector
@@ -15,6 +16,8 @@ import javax.inject.Singleton
                 AppModule::class, ViewModelFactoryModule::class]
 )
 interface AppComponent: AndroidInjector<BaseApplication> {
+
+    fun sessionManager(): SessionManager
 
     @Component.Builder
     interface Builder {
